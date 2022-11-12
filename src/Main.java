9049f6fc7e2cf40fc4a8054307b10061122dd01e
YagmurKaranfil <59535304+YagmurKaranfil@users.noreply.github.com>
2022-11-12 17:47:25 +0300

    Vucut_Kitle_Indexi

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import  java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        double kilo,boy, vucutIndexi;
+
+        Scanner input=new Scanner(System.in);
+        System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz : ");
+        boy=input.nextDouble();
+
+        System.out.print("Lütfen kilonuzu giriniz :");
+        kilo=input.nextDouble();
+
+        vucutIndexi= kilo/(boy*boy);
+
+        System.out.print("Vücut Kitle İndeksiniz : "+ vucutIndexi);
+    }
+}
